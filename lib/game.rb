8e46d1381e589606d1267474df7db95fcf267d4e
ljr5102 <ljr5102@gmail.com>
2016-06-14 11:56:21 -0400

    Add play method for game and start game upon running file

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,6 @@
+require './lib/board'
+require './lib/player'
+
 class Game
 
   attr_reader :player, :board
@@ -5,6 +8,7 @@ class Game
   def initialize(board, player)
     @player = player
     @board = board
+    self.play
   end
 
   def over?
@@ -13,8 +17,24 @@ class Game
 
   def play
     until over?
-      puts "you're stuck!"
+      system("clear")
+      puts board.display
+      guesses = player.guess
+      board.flip(guesses[0][0], guesses[0][1])
+      board.flip(guesses[1][0], guesses[1][1])
+      puts board.display
+      sleep(2)
+      unless board.matches(guesses[0], guesses[1])
+        board.flip(guesses[0][0], guesses[0][1])
+        board.flip(guesses[1][0], guesses[1][1])
+      end
     end
   end
 
+end
+
+if __FILE__ == $PROGRAM_NAME
+  new_board = Board.generate_board(:easy)
+  new_player = Player.new("PlayerOne")
+  new_game = Game.new(new_board, new_player)
 end
